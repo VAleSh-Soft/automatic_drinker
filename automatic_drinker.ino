006726559f88b01b216f39013fb943d9f6f33863
Shatalov Vladimir <valesh-soft@yandex.ru>
2025-01-06 10:54:16 +0300

    version 0.3

--- automatic_drinker.ino
+++ automatic_drinker.ino
@@ -20,9 +20,10 @@
 
 enum ModuleState
 {
-  DEFAULT_MODE,
-  CONTINOUS_MODE,
-  STANDBAY_MODE
+  DEFAULT_MODE,   // дефолтный режим работы
+  CONTINOUS_MODE, // режима непрерывной работы помпы
+  STANDBAY_MODE,  // спящий режима
+  PUMP_STOP_MODE  // режим остновки помпы из-за низкого уровня воды
 };
 
 // ===================================================
@@ -30,20 +31,44 @@ enum ModuleState
 shButton btn(BTN_PIN);
 shButton pir(PIR_SENSOR_PIN); // датчик движения обрабатываем как обычную кнопку
 
-shTaskManager tasks(2);
+shTaskManager tasks(3);
 
-shHandle pump_starting;
-shHandle pump_guard;
+shHandle pump_starting;       // включение режима работы помпы на пять минут
+shHandle pump_guard;          // собственно, отслеживание необхоимости работы помпы
+shHandle start_pump_by_timer; // периодическое включение режима работы помпы на пять минут
 
-ModuleState module_state = DEFAULT_MODE;
+ModuleState current_mode = DEFAULT_MODE;
 
 // ===================================================
 
+void setCurrentMode(ModuleState mode);
 void btnCheck();
 void pumpStaring();
 void pumpGuard();
+void startPumpByTimer();
 
 // ===================================================
+
+void setCurrentMode(ModuleState mode)
+{
+  current_mode = mode;
+  switch (current_mode)
+  {
+  case DEFAULT_MODE:
+    tasks.startTask(start_pump_by_timer);
+    tasks.stopTask(pump_starting);
+    break;
+  case CONTINOUS_MODE:
+    tasks.stopTask(start_pump_by_timer);
+    break;
+  case STANDBAY_MODE:
+    tasks.stopTask(start_pump_by_timer);
+    break;
+  case PUMP_STOP_MODE:
+    tasks.stopTask(start_pump_by_timer);
+    break;
+  }
+}
 
 void btnCheck()
 {
@@ -51,29 +76,28 @@ void btnCheck()
   {
   case BTN_LONGCLICK:
     // длинный клик - переключает дефолтный и спящий режимы
-    module_state = (module_state != STANDBAY_MODE) ? STANDBAY_MODE
-                                                   : DEFAULT_MODE;
+    (current_mode != STANDBAY_MODE) ? setCurrentMode(STANDBAY_MODE)
+                                    : setCurrentMode(DEFAULT_MODE);
     break;
   case BTN_ONECLICK:
     // одиночный клик
-    switch (module_state)
+    switch (current_mode)
     {
     case DEFAULT_MODE:
       // в дефолтном режиме включает помпу на пять минут, если она выключена, и наоборот
       pumpStaring();
       break;
-    case STANDBAY_MODE:
-    case CONTINOUS_MODE:
-      // если модуль в спящем или непрерывном режимах, переводит его в дефолтный режим
-      module_state = DEFAULT_MODE;
+    default:
+      // во всех остальных случаях переводит его в дефолтный режим
+      setCurrentMode(DEFAULT_MODE);
       break;
     }
     break;
   case BTN_DBLCLICK:
     // двойной клик в дефолтном режиме включает непрерывный режим
-    if (module_state == DEFAULT_MODE)
+    if (current_mode == DEFAULT_MODE)
     {
-      module_state = CONTINOUS_MODE;
+      setCurrentMode(CONTINOUS_MODE);
     }
     break;
   }
@@ -81,7 +105,7 @@ void btnCheck()
   switch (pir.getButtonState())
   {
   case BTN_DOWN:
-    if (module_state == DEFAULT_MODE)
+    if (current_mode == DEFAULT_MODE)
     {
       pumpStaring();
     }
@@ -98,13 +122,14 @@ void pumpStaring()
   else
   {
     tasks.stopTask(pump_starting);
+    tasks.startTask(start_pump_by_timer);
   }
 }
 
 void pumpGuard()
 {
   uint8_t pump_state = LOW;
-  switch (module_state)
+  switch (current_mode)
   {
   case DEFAULT_MODE:
     pump_state = tasks.getTaskState(pump_starting);
@@ -119,11 +144,20 @@ void pumpGuard()
   digitalWrite(MOTOR_PIN, pump_state);
 }
 
+void startPumpByTimer()
+{
+  if (current_mode == DEFAULT_MODE && !tasks.getTaskState(pump_starting))
+  {
+    tasks.startTask(pump_starting);
+  }
+}
+
 // ===================================================
 
 void setup()
 {
   btn.setVirtualClickOn();
+  pir.setTimeoutOfDebounce(0);
 
   // ===================================================
 
@@ -135,12 +169,12 @@ void setup()
   pinMode(PWR_OFF_LED_PIN, OUTPUT);
   pinMode(L_LEVEL_SENSOR_PIN, INPUT);
   pinMode(H_LEVEL_SENSOR_PIN, INPUT);
-  pinMode(PIR_SENSOR_PIN, INPUT);
 
   // ===================================================
 
   pump_starting = tasks.addTask(300000ul, pumpStaring, false);
   pump_guard = tasks.addTask(5ul, pumpGuard);
+  start_pump_by_timer = tasks.addTask(1800000ul, startPumpByTimer);
 }
 
 void loop()
